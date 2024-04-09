7640dab29604a4626a48b41cc66eacc69dbcdb1b
Lirae Que Data <liraedata59@gmail.com>
2024-04-10 01:48:27 +0800

    Update init_django method in main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,60 @@
 #!/usr/bin/bash
 
 
-init_django() {
+init_django() { 
+    local root_dir="$1"
 
+    local venv_path="$root_dir/venv"
+    local config_path="$root_dir/config"
+    local api_path="$root_dir/api"
+    local v1_path="$api_path/v1"
+
+    local directories=(
+        "models" "tests" "admin" "viewsets" 
+        "serializers" "permissions"
+    )
+
+    echo "Initializing Django project..."
+
+    mkdir -p "$root_dir" && cd "$root_dir" || exit 1
+
+    virtualenv "$venv_path" || exit 1
+    source "$venv_path/Scripts/activate" || exit 1
+
+    pip install django djangorestframework \
+                djangorestframework-simplejwt \
+                django-cors-headers \
+                drf-yasg pydantic \
+                python-dotenv || exit 1
+
+    pip freeze > requirements.txt || exit 1
+    touch README.md .gitignore .dockerignore Dockerfile || exit 1
+    
+    django-admin startproject config . || exit 1
+    cd "$config_path" || exit 1
+
+    mkdir "settings" && mv settings.py "settings/base.py" || exit 1
+    cd "settings" || exit 1
+    touch "__init__.py" "local.py" "production.py" || exit 1
+
+    cd "$root_dir" || exit 1
+    mkdir "$api_path" && cd "$api_path" || exit 1
+    touch "__init__.py" || exit 1
+    django-admin startapp "v1" || exit 1
+    cd "$v1_path" || exit 1
+    rm "tests.py" "views.py" "admin.py" "models.py" || exit 1
+
+    for directory in "${directories[@]}"; do 
+        mkdir "$v1_path/$directory" && cd "$v1_path/$directory" || exit 1
+        touch "__init__.py" || exit 1
+    done
+
+    deactivate
 }
 
 
 init_git() {
-
+    echo "Initializing Git..."
 }
 
 
@@ -27,6 +74,11 @@ main() {
     done
 
 
+    project_path="$(pwd)/$project_name"
+    
+
+
+    init_django "$project_path"
 
 }
 
