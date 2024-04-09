9fa4d56f15060214b3573149071488965c1bc259
Lirae Que Data <liraedata59@gmail.com>
2024-04-10 00:58:54 +0800

    Add user input in main.sh

--- main.sh
+++ main.sh
@@ -2,10 +2,20 @@
 
 
 
-
 main() {
 
-    read -p "Enter project name: " project_name
+    until [ -n "$project_name" ]; do
+        clear
+
+        read -p "Enter project name: " project_name
+
+        if [ -z "$project_name" ]; then 
+            read -p "Do you want to continue? [Y/n] " yn
+            if [[ "$yn" = [Nn]* ]]; then 
+                exit 0
+            fi
+        fi
+    done
 
 
 }
