334f7743daa8f735eac33ec8fe4103644011c444
Lirae Que Data <liraedata59@gmail.com>
2024-04-10 17:06:30 +0800

    Add user input in main.sh

--- main.sh
+++ main.sh
@@ -91,39 +91,6 @@ init_django() {
     deactivate
 }
 
-usage() {
-    clear
-
-    cat <<- EOF
-
-		${GREEN}╺┳┓ ┏┏━┓┏┓╻┏━┓┏━┓${WHITE}┏┳┓┏━┓┏┳┓╻┏━╸
-		${GREEN} ┃┃ ┃┣━┫┃┗┫┃ ┓┃ ┃${WHITE}┃┃┃┣━┫ ┃ ┃┃  
-		${GREEN}╺┻┛┗┛╹ ╹╹ ╹┗━┛┗━┛${WHITE}╹ ╹╹ ╹ ╹ ╹┗━╸
-		
-		Djangomatic V1.0 : Generate Django project structure.
-		Developed By     : Lirae Que Data (@WannaCry081)
-			
-		Usage : django [-h] [-m | -r | -n | -mr] [project_name] [-g] [-p] [repository_link]
-
-		Options:
-		   -h	Show this help message
-		   -n	Use Django Ninja instead of Django Rest Framework
-		   -m	Use MVT Architecture for the project template
-		   -r	Use REST API for the project template
-		   -mr	Use both MVT and REST for the project template
-		   -g	Initialize version control
-		   -p	Push project to a public repository
-		   
-	EOF
-
-    cat <<- EOF
-		Examples: 
-		    django -r note_api     Create 'note_api' django API template
-		    django -m todo_app -g  Create 'todo_app' django MVT and initialize git
-		
-
-	EOF
-}
 
 
 init_git() {
@@ -132,25 +99,106 @@ init_git() {
 
 
 main() {
+    local h_flag=false
+    local m_flag=false
+    local n_flag=false
+    local r_flag=false
+    local g_flag=false
+    local p_flag=false
+    local cmd_line="${GREEN}djm${WHITE}"
 
-    local tool_line="${GREEN}djm${WHITE}"
+    clear && usage
 
-    until [ -n "$project_name" ]; do
-        usage
-        read -p "${tool_line} > " project_name
+    while [ true ]; do
+        read -p "$cmd_line > " command args
 
-        if [ -z "$project_name" ]; then 
-            read -p "${tool_line} Do you want to continue? [Y/n] " yn
-            if [[ "$yn" = [Nn]* ]]; then 
-                exit 0
+        if [ -n "$args" ] && [ "$command" = "django" ]; then
+
+            IFS=' ' read -r -a parts <<< "$args"
+            for arg in "${parts[@]}"; do
+                case "$arg" in
+                    -h)
+                        h_flag=true
+                        ;;
+                    -m)
+                        m_flag=true
+                        ;;
+                    -r)
+                        r_flag=true
+                        ;;
+                    -n)
+                        n_flag=true
+                        ;;
+                    -g)
+                        g_flag=true
+                        ;;
+                    -p)
+                        p_flag=true
+                        ;;
+                    *)
+                        if [ -z "$project_name" ]; then
+                            project_name="$arg"
+                        elif [ -z "$repository_link" ]; then
+                            repository_link="$arg"
+                        fi
+                        ;;
+                esac
+            done
+            
+            if [ "$h_flag" ]; then 
+                usage
+                continue
             fi
-        fi
+
+            if [ "$r_flag" ]; then  
+                if [ "$m_flag" ]; then 
+
+                else 
+
+                fi
+
+                if [ "$g_flag" ]; then 
+
+                fi
+
+                if [ "$p_flag" ]; then 
+
+                fi
+
+                continue
+            done
+
+            if [ "$n_flag" ]; then 
+                if [ "$m_flag" ]; then 
+
+                else 
+                
+                fi
+
+                if [ "$g_flag" ]; then 
+
+                fi
+
+                if [ "$p_flag" ]; then 
+
+                fi
+
+                continue
+            done
+
+        elif [ -n "$command" ]; then
+            if [ "$command" = "exit" ]; then 
+                echo "Happy Hacking!"
+                exit 0
+            elif [ "$command" = "clear" ]; then 
+                clear 
+            elif [[ "$command" = "help" || "$command" = "django" ]]; then 
+                usage
+            else
+                echo "$cmd_line: $command: command not found"
+            fi
+        fi 
     done
-
-    local project_path="$(pwd)/$project_name"
-
-    init_django "$project_path"
-
 }
 
 
