69e5ae7f1b855a56d9b00e0d925640f1a51b6e24
Lirae Que Data <liraedata59@gmail.com>
2024-04-11 00:44:16 +0800

    Update init_django_ninja implementation

--- main.sh
+++ main.sh
@@ -129,7 +129,61 @@ init_django_rest() {
 
 
 init_django_ninja() {
-    echo "Ninja"
+    local root_dir="$(pwd)/$2"
+
+    local venv_path="$root_dir/venv"
+    local api_path="$root_dir/api"
+    local v1_path="$api_path/v1"
+    local config_path="$root_dir/config"
+
+    local directories=(
+        "models" "tests" "admin" "controllers" 
+        "services" "repositories" "utils" "exceptions"
+        "schemas"
+    )
+
+    echo "Initializing Django project..."
+
+    mkdir -p "$root_dir" && cd "$root_dir" || exit 1
+
+    virtualenv "$venv_path" || exit 1
+    source "$venv_path/Scripts/activate" || exit 1
+
+    pip install django django-ninja-extra \
+                django-cors-headers \
+                python-dotenv || exit 1
+
+    pip freeze > requirements.txt || exit 1
+    touch README.md .gitignore .dockerignore Dockerfile || exit 1
+
+    echo -e "*.db\n*.sqlite3\n**/__pycache\n/venv" > .gitignore || exit 1
+    
+    django-admin startproject config . || exit 1
+    cd "$config_path" || exit 1
+
+    mkdir "settings" && mv settings.py "settings/base.py" || exit 1
+    cd "settings" || exit 1
+    touch "__init__.py" "local.py" "production.py" || exit 1
+
+    cd "$root_dir" || exit 1
+    mkdir "$api_path" && cd "$api_path" || exit 1
+    touch "__init__.py" || exit 1
+    django-admin startapp "v1" || exit 1
+    cd "$v1_path" || exit 1
+    rm "tests.py" "views.py" "admin.py" "models.py" || exit 1
+    touch urls.py || exit 1
+
+    for directory in "${directories[@]}"; do 
+        mkdir "$v1_path/$directory" && cd "$v1_path/$directory" || exit 1
+        touch "__init__.py" || exit 1
+    done
+
+    cd "$root_dir" || exit 1
+
+    if [ "$1" = true ]; then 
+        init_django_mvt "$root_dir"
+    fi
+    deactivate
 }
 
 
